9e4cea8adc0d5f31bdedcb1631fd8a220396c51b
Andrzej Pisarek <andrzej.r.pisarek@gmail.com>
2017-04-11 16:27:01 +0200

    Add allPaths

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
 import Lib
+import System.Directory
 
 main :: IO ()
-main = someFunc
+main = putStrLn "ELO"
+
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,18 @@
 module Lib
     ( someFunc
+    , allPaths
     ) where
+
+import System.Directory
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+rootPath = "./data/lingspam_public/bare/"
+
+allPaths = do
+  partFoldersNames <- listDirectory rootPath
+  let partFolderPaths = map (\folder -> rootPath ++ folder) partFoldersNames
+  innerFiles <- mapM listDirectory partFolderPaths
+  return $ concat $ zipWith (\path files -> map (\file -> path ++ "/" ++ file) files) partFolderPaths innerFiles
+
