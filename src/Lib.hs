0c93837e8adf80ecc7d0ac8c62723f2c7ee471a4
Andrzej Pisarek <andrzej.r.pisarek@gmail.com>
2017-04-13 22:36:03 +0200

    Add functions for reading and preprocessing emails

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,16 @@
 module Lib
     ( someFunc
     , allPaths
+    , spamPaths
+    , preprocessEmail
+    , spamMails
+    , allMails
+    , rmdups
     ) where
 
 import System.Directory
+import Data.List
+import Data.Char
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
@@ -16,3 +23,41 @@ allPaths = do
   innerFiles <- mapM listDirectory partFolderPaths
   return $ concat $ zipWith (\path files -> map (\file -> path ++ "/" ++ file) files) partFolderPaths innerFiles
 
+spamPaths = do
+  paths <- allPaths
+  return $ filter (\path -> isInfixOf "spmsg" path) paths
+
+nonSpamPaths = do
+  paths <- allPaths
+  return $ filter (\path -> not $ isInfixOf "spmsg" path) paths
+
+spamMails = do
+  paths <- spamPaths
+  mapM readFile paths
+
+nonSpamMails = do
+  paths <- nonSpamPaths
+  mapM readFile paths
+
+allMails = do
+  spam <- spamMails
+  nonSpam <- nonSpamMails
+  return $ map preprocessEmail (spam ++ nonSpam)
+
+
+
+filterAlpha = filter isAlpha
+containsAlpha = any isAlpha
+toLowerWord = map toLower
+lengthGreaterThanTwo word = length word > 2
+
+rmdups :: (Ord a) => [a] -> [a]
+rmdups = map head . group . sort
+
+preprocessEmail email =
+  (map toLowerWord) .
+  (filter lengthGreaterThanTwo) .
+  (map filterAlpha) .
+  (filter containsAlpha) .
+  tail . -- first word is "Subject:" in every email in this dataset, so we omit it
+  words $ email
